52bc40e435ce7a3daba841a170b7eccdd1d4f1b9
shourovr82 <shafinur512@gmail.com>
2023-11-19 22:28:36 +0600

    [BUG] bug fixed

--- src/app/(dashboardlayout)/dashboard/page.tsx
+++ src/app/(dashboardlayout)/dashboard/page.tsx
@@ -1,35 +1,14 @@
-"use client";
+import { Metadata } from "next";
 
-import TotalIncomeChart from "@/components/dashboard/UI/TotalIncomeChart";
-import CardSection from "@/components/dashboard/UI/cardSection";
-import LatestOrderAndStock from "@/components/dashboard/UI/latestOrderAndStock";
-import OrderChart from "@/components/dashboard/UI/orderChart";
-import UserSection from "@/components/dashboard/UI/userSection";
+export const metadata: Metadata = {
+  title: "Dashboard | Pizza-Prisma",
+  description: "shourovr82 | pizza-prism | Shafinur Islam",
+};
 
 const DashboardHomePage = () => {
   return (
     <>
-      <div>
-        {/*  */}
-        <div className="relative font-sans">
-          {/* Main Layout for Dashboard */}
-          <main className=" w-full p-5 bg-[#fafafa]">
-            <div className="">
-              <UserSection />
-              <CardSection />
-              <div className="grid grid-cols-8  mt-6  gap-5">
-                <div className="col-span-5">
-                  <OrderChart />
-                </div>
-                <div className="col-span-3">
-                  <TotalIncomeChart />
-                </div>
-              </div>
-              <LatestOrderAndStock />
-            </div>
-          </main>
-        </div>
-      </div>
+      <div></div>
     </>
   );
 };
