724dfb5af75a0f6241f14e3b51abc336ee5cbf89
shourovr82 <shafinur512@gmail.com>
2023-12-18 00:13:05 +0600

    [DONE] add order 8 tables added with structured design

---
+++ src/components/dashboard/orders/OrderTables.tsx
@@ -0,0 +1,312 @@
+import React from "react";
+
+const OrderTables = () => (
+  <section>
+    <div className="px-3 py-3">
+      <div className="grid grid-cols-6 2xl:gap-20 ">
+        {/* left  tables ------------------------------- */}
+        <div className="col-span-2 grid grid-cols-2 2xl:gap-x-16 space-y-5 ">
+          <div className="col-span-2 grid grid-cols-2">
+            {/* Table T12 */}
+            <div className="col-span-1 border">
+              {/* Top */}
+              <div className="flex justify-center gap-5 items-center">
+                <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+                <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+              </div>
+
+              {/* Middle */}
+              <div className="grid grid-cols-7 gap-2 p-2">
+                <div className="col-span-1 space-y-2 flex flex-col items-center justify-center">
+                  <div className="w-[20px] h-[50px] border bg-white rounded-full" />
+                  <div className="w-[20px] h-[50px] border bg-white rounded-full" />
+                </div>
+                <div className="col-span-5 w-full flex justify-center">
+                  <div className="bg-white border rounded-xl shadow-2xl shadow-black/10 flex justify-center items-center w-[130px] h-[130px]">
+                    <div>
+                      <p className="p-3 bg-[#ebfcd0] text-green-600 font-medium rounded-full">T12</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-span-1 space-y-2 flex flex-col items-center justify-center">
+                  <div className="w-[20px] h-[50px] border bg-white rounded-full" />
+                  <div className="w-[20px] h-[50px] border bg-white rounded-full" />
+                </div>
+              </div>
+
+              {/* Bottom */}
+              <div className="flex justify-center gap-5 items-center">
+                <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+                <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+              </div>
+            </div>
+            {/* Table T13 */}
+            <div className="col-span-1 border">
+              {/* Top */}
+              <div className="flex justify-center gap-5 items-center">
+                <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+                <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+              </div>
+
+              {/* Middle */}
+              <div className="grid grid-cols-7 gap-2 p-2">
+                <div className="col-span-1 space-y-2 flex flex-col items-center justify-center">
+                  <div className="w-[20px] h-[50px] border bg-white rounded-full" />
+                </div>
+                <div className="col-span-5 w-full flex justify-center">
+                  <div className="bg-white border rounded-full shadow-2xl shadow-black/10 flex justify-center items-center w-[130px] h-[130px]">
+                    <div>
+                      <p className="p-3 bg-[#ebfcd0] text-green-600 font-medium rounded-full">T13</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-span-1 space-y-2 flex flex-col items-center justify-center">
+                  <div className="w-[20px] h-[50px] border bg-white rounded-full" />
+                  <div className="w-[20px] h-[50px] border bg-white rounded-full" />
+                </div>
+              </div>
+
+              {/* Bottom */}
+              <div className="flex justify-center gap-5 items-center">
+                <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+                <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+              </div>
+            </div>
+          </div>
+          {/* table 17 */}
+          <div className="col-span-2 border  ">
+            {/* Table T14 */}
+            <div className="col-span-1 ">
+              {/* Top */}
+              <div className="flex justify-center gap-5 items-center">
+                <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+                <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+                <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+                <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+              </div>
+
+              {/* Middle */}
+              <div className="grid grid-cols-7 gap-2 p-2">
+                {/* left */}
+                <div className="col-span-1   space-y-2 flex flex-col items-center justify-center">
+                  <div className="w-[20px] h-[50px] border bg-white rounded-full" />
+                </div>
+                <div className="col-span-5 w-full flex justify-center">
+                  <div className="bg-white border rounded-full shadow-2xl shadow-black/10 flex justify-center items-center w-full h-[130px]">
+                    <div>
+                      <p className="p-3 bg-[#fcf3c9] text-[#eda71f] font-medium rounded-full">T14</p>
+                    </div>
+                  </div>
+                </div>
+                {/* right chair */}
+                <div className="col-span-1 space-y-2 flex flex-col items-center justify-center">
+                  <div className="w-[20px] h-[50px] border bg-white rounded-full" />
+                </div>
+              </div>
+
+              {/* Bottom */}
+              <div className="flex justify-center gap-5 items-center">
+                <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+                <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+                <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+                <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* middle tables ------------------------------- */}
+        <div className="col-span-2   space-y-5  ">
+          {/* Table T14 */}
+          <div className="col-span-1 border">
+            {/* Top */}
+            <div className="flex justify-center gap-5 items-center">
+              <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+              <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+              <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+              <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+            </div>
+
+            {/* Middle */}
+            <div className="grid grid-cols-7 gap-2 p-2">
+              {/* left */}
+              <div className="col-span-1   space-y-2 flex flex-col items-center justify-center">
+                <div className="w-[20px] h-[50px] border bg-white rounded-full" />
+              </div>
+              <div className="col-span-5 w-full flex justify-center">
+                <div className="bg-white border rounded-full shadow-2xl shadow-black/10 flex justify-center items-center w-full h-[130px]">
+                  <div>
+                    <p className="p-3 bg-[#fcf3c9] text-[#eda71f] font-medium rounded-full">T14</p>
+                  </div>
+                </div>
+              </div>
+              {/* right chair */}
+              <div className="col-span-1 space-y-2 flex flex-col items-center justify-center">
+                <div className="w-[20px] h-[50px] border bg-white rounded-full" />
+              </div>
+            </div>
+
+            {/* Bottom */}
+            <div className="flex justify-center gap-5 items-center">
+              <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+              <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+              <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+              <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+            </div>
+          </div>
+          {/* table 18 */}
+          <div className="col-span-2 border  ">
+            {/* Table T18 */}
+
+            {/* Top */}
+            <div className="flex justify-center gap-5 items-center">
+              <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+              <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+              <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+              <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+            </div>
+
+            {/* Middle */}
+            <div className="grid grid-cols-7 gap-2 p-2">
+              {/* left */}
+              <div className="col-span-1   space-y-2 flex flex-col items-center justify-center">
+                <div className="w-[20px] h-[50px] border bg-white rounded-full" />
+              </div>
+              <div className="col-span-5 w-full flex justify-center">
+                <div className="bg-white border rounded-full shadow-2xl shadow-black/10 flex justify-center items-center w-full h-[130px]">
+                  <div>
+                    <p className="p-3 bg-[#fcf3c9] text-[#eda71f] font-medium rounded-full">T18</p>
+                  </div>
+                </div>
+              </div>
+              {/* right chair */}
+              <div className="col-span-1 space-y-2 flex flex-col items-center justify-center">
+                <div className="w-[20px] h-[50px] border bg-white rounded-full" />
+              </div>
+            </div>
+
+            {/* Bottom */}
+            <div className="flex justify-center gap-5 items-center">
+              <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+              <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+              <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+              <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+            </div>
+          </div>
+        </div>
+
+        {/* right tables -------------------------------*/}
+        <div className="col-span-2 grid grid-cols-2 2xl:gap-x-16 space-y-5">
+          <div className="col-span-2 grid grid-cols-2">
+            {/* Table T15 */}
+            <div className="col-span-1 border">
+              {/* Top */}
+              <div className="flex justify-center gap-5 items-center">
+                <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+                <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+              </div>
+
+              {/* Middle */}
+              <div className="grid grid-cols-7 gap-2 p-2">
+                <div className="col-span-1 space-y-2 flex flex-col items-center justify-center">
+                  <div className="w-[20px] h-[50px] border bg-white rounded-full" />
+                </div>
+                <div className="col-span-5 w-full flex justify-center">
+                  <div className="bg-white border rounded-full shadow-2xl shadow-black/10 flex justify-center items-center w-[130px] h-[130px]">
+                    <div>
+                      <p className="p-3 bg-[#eeeef0] text-black/40 font-medium rounded-full">T15</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-span-1 space-y-2 flex flex-col items-center justify-center">
+                  <div className="w-[20px] h-[50px] border bg-white rounded-full" />
+                  <div className="w-[20px] h-[50px] border bg-white rounded-full" />
+                </div>
+              </div>
+
+              {/* Bottom */}
+              <div className="flex justify-center gap-5 items-center">
+                <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+                <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+              </div>
+            </div>
+            {/* Table T16 */}
+            <div className="col-span-1 border">
+              {/* Top */}
+              <div className="flex justify-center gap-5 items-center">
+                <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+                <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+              </div>
+
+              {/* Middle */}
+              <div className="grid grid-cols-7 gap-2 p-2">
+                <div className="col-span-1 space-y-2 flex flex-col items-center justify-center">
+                  <div className="w-[20px] h-[50px] border bg-white rounded-full" />
+                  <div className="w-[20px] h-[50px] border bg-white rounded-full" />
+                </div>
+                <div className="col-span-5 w-full flex justify-center">
+                  <div className="bg-white border rounded-xl shadow-2xl shadow-black/10 flex justify-center items-center w-[130px] h-[130px]">
+                    <div>
+                      <p className="p-3 bg-[#ffe5d7] text-[#ff3d3a] font-medium rounded-full">T16</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-span-1 space-y-2 flex flex-col items-center justify-center">
+                  <div className="w-[20px] h-[50px] border bg-white rounded-full" />
+                  <div className="w-[20px] h-[50px] border bg-white rounded-full" />
+                </div>
+              </div>
+
+              {/* Bottom */}
+              <div className="flex justify-center gap-5 items-center">
+                <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+                <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+              </div>
+            </div>{" "}
+          </div>
+          {/* table 18 */}
+          <div className="col-span-2 border  ">
+            {/* Table T18 */}
+
+            {/* Top */}
+            <div className="flex justify-center gap-5 items-center">
+              <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+              <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+              <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+              <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+            </div>
+
+            {/* Middle */}
+            <div className="grid grid-cols-7 gap-2 p-2">
+              {/* left */}
+              <div className="col-span-1   space-y-2 flex flex-col items-center justify-center">
+                <div className="w-[20px] h-[50px] border bg-white rounded-full" />
+              </div>
+              <div className="col-span-5 w-full flex justify-center">
+                <div className="bg-white border rounded-full shadow-2xl shadow-black/10 flex justify-center items-center w-full h-[130px]">
+                  <div>
+                    <p className="p-3 bg-[#fcf3c9] text-[#eda71f] font-medium rounded-full">T18</p>
+                  </div>
+                </div>
+              </div>
+              {/* right chair */}
+              <div className="col-span-1 space-y-2 flex flex-col items-center justify-center">
+                <div className="w-[20px] h-[50px] border bg-white rounded-full" />
+              </div>
+            </div>
+
+            {/* Bottom */}
+            <div className="flex justify-center gap-5 items-center">
+              <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+              <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+              <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+              <div className="w-[50px] h-[20px] border bg-white rounded-full" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+);
+
+export default OrderTables;
---
+++ src/components/dashboard/tables/DashboardTablesList.tsx
@@ -0,0 +1,9 @@
+const DashboardTablesLists = () => (
+  <section className="mt-5">
+    <div>
+      <h2 className="text-xl text-[#333333] font-semibold">Choose Table</h2>
+    </div>
+  </section>
+);
+
+export default DashboardTablesLists;
